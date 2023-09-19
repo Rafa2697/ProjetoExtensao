ffed6f62696efd6e84d6eef619ce85641d78fc1d
Rafael <Rafael.mattos468@gmail.com>
2023-09-19 16:47:08 -0300

    update

---
+++ questions.js
@@ -0,0 +1,167 @@
+export default [
+  {
+    question: "O que é CPF e qual sua importância?",
+    answers: [
+      { option: "Cadastro de pessoas nacionais, armazena informações apenas de brasileiros", correct: true },
+      { option: "Cadastro de pessoas físicas, registro que armazena informações cadastrais de todos os cidadãos inscritos e é gerenciado pela receita federal.", correct: false },
+      { option: "Documento de Identidade, que garante os direitos ao cidadão.", correct: false },
+    ],
+  },
+  {
+    question: "O que é contrabando?",
+    answers: [
+      { option: "Importar ou exportar mercadorias proíbidas no País.", correct: false },
+      { option: "Vender mercadorias proíbidas.", correct: true },
+      
+    ],
+  },
+  {
+    question: "Qual a diferença entre tributo e imposto?",
+    answers: [
+      { option: "Tributo é gênero, imposto é espécie, ou seja, imposto é um tipo de tributo. Outros exemplos de tributo são: taxas e contruições.", correct: true },
+      { option: "Tributo é uma doação, imposto é obrigatório pagar.", correct: false },
+      
+    ],
+  },
+  {
+    question: "Qual a importancia do CNPJ para o consumidor?",
+    answers: [
+      { option: "Identificar a veracidade do seu negócio.", correct: false },
+      { option: "Adquirir produtos e serviços de empresas devidamente formalizadas ajuda a garantir seus direitos de consumidor.", correct: true },
+      
+    ],
+  },
+  {
+    question: "O que é descaminho?",
+    answers: [
+      { option: "Entregar mercadorias em endereço errado.", correct: true },
+      { option: "Importar, exportar ou consumir mercadorias permitidas no País, mas sem o pagamento dos impostos.", correct: false },
+      
+    ],
+  },
+  {
+    question: "O que é sonegação?",
+    answers: [
+      { option: "Omitir ou fazer declaração incorreta com intenção de deixar de pagar os tribudos devidos, sendo essa conduta um crime.", correct: true },
+      { option: "Deixar de pagar impostos.", correct: false },
+      
+    ],
+  },
+  {
+    question: "O que é CNPJ e qual a sua importancia?",
+    answers: [
+      { option: "Cadastro nacional de pessoa jurídica, obrigatório para o exercício das atividades da empresa.", correct: true },
+      { option: "Cadastro nacional de pessoas jovens, obrigatório para toda pessoa maior de 18 anos", correct: false },
+      
+    ],
+  },
+  {
+    question: "O que é contrafação?",
+    answers: [
+      { option: "Qualquer reprodução ou uso não autorizado de marcas registradas, conhecido como pirataria.", correct: true },
+      { option: "Venda ilegal de produtos", correct: false },
+      
+    ],
+  },
+  {
+    question: "Qual é a função da nota fiscal?",
+    answers: [
+      { option: "Garantir que a venda do produto será declarada, evitando a sonegação, além de garantir os direitos do consumidor.", correct: true },
+      { option: "Comprovar que o consumidor comprou o produto, apenas.", correct: false },
+      
+    ],
+  },
+  {
+    question: "Qual é a função da nota fiscal?",
+    answers: [
+      { option: "Garantir que a venda do produto será declarada, evitando a sonegação, além de garantir os direitos do consumidor.", correct: true },
+      { option: "Comprovar que o consumidor comprou o produto, apenas.", correct: false },
+      
+    ],
+  },
+  {
+    question: "Como e feita a divisão administrativa da Receita Federal?",
+    answers: [
+      { option: "Em dez regiões fiscais, exemplo: O estado de São Paulo corresponde à 8ª região fiscal. Algumas regiões são compostas por mais de um estado.", correct: true },
+      { option: "Em Vinte e uma regiões fiscais, exemplo: O estado de São Paulo corresponde à 8ª região fiscal. Uma para cada estado.", correct: false },
+      
+    ],
+  },
+  {
+    question: "Onde se dá a principal atuação da Aduana?",
+    answers: [
+      { option: "Sua principal atuação é nas fronteiras estaduais do país, sendo elas terrestres, aéreas ou portuárias, chamadas de zonas primárias.", correct: false },
+      { option: "Sua principal atuação é nas fronteiras do país, sendo elas terrestres, aéreas ou portuárias, chamadas de zonas primárias.", correct: true },
+      
+    ],
+  },
+  {
+    question: "Quem pode destinar parte de seu imposto para os fundos do Direito da Cirança e do Adolescente?",
+    answers: [
+      { option: "Cidadãos e empresas. Por exemplo, cada pessoa pode doar até 6% do seu imposto de Renda devido no ano.", correct: true},
+      { option: "Governo. Com a arrecadação do imposto, parte do dinheiro é investido.", correct: false },
+      
+    ],
+  },
+  {
+    question: "Crianças podem ter CPF?",
+    answers: [
+      { option: "Não. Apenas maiores de 18 anos.", correct: false},
+      { option: "Sim, desde que já tenha documento de indentidade.", correct: false },
+      { option: "Sim. De qualquer idade, inclusive recem-nascidos.", correct: true},
+      { option: "Não. Apenas maiores de 10 anos.", correct: false},
+      
+    ],
+  },
+  {
+    question: "O que faz a Aduana?",
+    answers: [
+      { option: "Controla e fiscaliza o fluxo de mercadorias importadas no país.", correct: false},
+      { option: "Controla e fiscaliza o fluxo internacional de bens, mercadorias e veículos. No Brasil, é a Receita feretal que excerce a Administração Aduaneira.", correct: true },
+    ],
+  },
+  {
+    question: "O que é a malha fiscal?",
+    answers: [
+      { option: "Quando tudo está correto em uma declaração.", correct: false},
+      { option: "É quando uma declaração apresenta pendências, por informações incorretas, omissão de dados ou necessidade de apresentação de documentos.", correct: true },
+    ],
+  },
+  {
+    question: "Quais as principais funções da Receita Federal do Brasil?",
+    answers: [
+      { option: "A administração de impostos e onde investir tal arrecadação.", correct: true },
+      { option: "A administração dos tributos federais, inclusive os previdenciarios e os incidentes sobre o comércio exterior.", correct: false },
+    ],
+  },
+  {
+    question: "Quais são as principais consequências da pirataria?",
+    answers: [
+      { option: "Prejudicar empresas que fazem os produtos originais, apenas.", correct: false },
+      { option: "Pode causar danos à saúdem prejudicar o mercado de trabalho formal e interferir no crescimento e na valorização do produto original.", correct: true },
+    ],
+  },
+  {
+    question: "O que é orçamento público?",
+    answers: [
+      { option: "Todo o dinheiro arrecadado por meio de impostos na sociedade.", correct: false },
+      { option: "É um compromisso, por meio de lei, entre governo e sociedade, com a previsão de receitas e despesas públicas.", correct: true },
+    ],
+  },
+  {
+    question: "A qual órgão a receita federal é subordinada?",
+    answers: [
+      { option: "Ministério da Agricultura.", correct: false },
+      { option: "Ministério da Educação.", correct: false },
+      { option: "Ministério da Economia.", correct: true},
+    ],
+  },
+  {
+    question: "Pirata é crime?",
+    answers: [
+      { option: "Não, pois o produto ainda será o mesmo, apenas com algumas mudanças.", correct: false },
+      { option: "Sim, podendo estar relacionada a outros crimes como sonegação de impostos, lavagem de dinheiro, corrupção, dentre outros.", correct: true },
+    ],
+  },
+  
+];
