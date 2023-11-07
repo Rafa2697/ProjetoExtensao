aa8b7606794a003e62cfb3473c267d0820ea68c7
Rafael <Rafael.mattos468@gmail.com>
2023-11-06 21:48:57 -0300

    update

---
+++ db.js
@@ -0,0 +1,13 @@
+import postgres from "postgres";
+
+const sql = postgres({
+    host: PGHOST,
+    database: PGDATABASE,
+    username: PGUSER,
+    password: PGPASSWORD,
+    port: 5432,
+    ssl: 'require',
+    connection: {
+      options: `project=${ENDPOINT_ID}`,
+    },
+})
